dbce6df4ce2eea6e9b0e6d67dcb401dc66d46f20
seanhill <seamhill93@gmail.com>
2022-10-16 16:27:02 -0400

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup
 setup(
     name='ManifoldBm',
     version='1.0',
-    packages=[''],
+    packages=['ManifoldBm'],
     url='https://github.com/shill1729/ManifoldBm',
     license='MIT',
-    author='seanhill',
-    author_email='',
+    install_requires=[
+          'numpy',
+          'sympy',
+          "matplotlib"
+    ],
+    author='S. Hill',
+    author_email='52792611+shill1729@users.noreply.github.com',
     description='Tools for Brownian motion on Manifolds'
 )
