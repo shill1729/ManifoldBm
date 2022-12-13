6a6169e56a4fba2e9797c8801cb96f0850319ca9
seanhill <seamhill93@gmail.com>
2022-12-13 15:29:29 -0500

    edit imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ManifoldBm',
-    version='1.2',
+    version='1.3',
     packages=['ManifoldBm'],
     url='https://github.com/shill1729/ManifoldBm',
     license='MIT',
